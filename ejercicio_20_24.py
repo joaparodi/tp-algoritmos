9043bd4ba807e406760f91f8e74cf9d8fd0bc889
joaquin <joaquinparodi79@gmail.com>
2026-05-03 20:41:01 -0300

    feat: add comment header for robot movement tracking algorithm

--- ejercicio_20_24.py
+++ ejercicio_20_24.py
@@ -1,5 +1,7 @@
 
 
+#actividades del tp: pila
+
 #20. Realizar un algoritmo que registre los movimientos de un robot, los datos que se guardan son
 #cantidad de pasos y dirección –suponga que el robot solo puede moverse en ocho direcciones:
 #norte, sur, este, oeste, noreste, noroeste, sureste y suroeste–. Luego desarrolle otro algoritmo
